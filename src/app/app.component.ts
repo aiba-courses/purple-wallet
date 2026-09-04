c5288100d2889acac74108b9f8e8d95719605b01
aiba-courses <abastrikov.courses@yandex.ru>
2026-09-04 16:54:08 +0300

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  imports: [RouterOutlet],
+  selector: 'app-root',
+  styleUrl: './app.scss',
+  templateUrl: './app.html',
+})
+export class AppComponent {
+  protected readonly title = signal('purple-wallet');
+}
